323a04c6dd2fe2416c00e01bd9b96c5050cae35e
Jeremy Santana <santanajay21@gmail.com>
2024-08-05 18:36:33 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+
+#check what type of data type it is 
+#then subscript each one and separate them while now making them and integer 
+#make a new variable and add them together 
+
+
+digit_one = int(two_digit_number[0])
+digit_two = int(two_digit_number[1])
+new_digit = digit_one + digit_two
+print(new_digit)
